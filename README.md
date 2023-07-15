46b2d5b8c15af0bd541c464a0521b56c96791939
Soul_Master <Soul-Master@users.noreply.github.com>
2023-07-15 17:54:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## To create `@web/dev-server`
+```
+npm start
+```
+
+## To generate Rollup output
+```
+npm run release
+```
---
+++ common.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SingOnMethod = exports.NetworkTypes = void 0;
+
+var SingOnMethod;
+(function (SingOnMethod) {
+    SingOnMethod["Iframe"] = "iframe";
+    SingOnMethod["Popup"] = "popup";
+    SingOnMethod["Tab"] = "tab";
+})(SingOnMethod = exports.SingOnMethod || (exports.SingOnMethod = {}));
+var NetworkTypes;
+(function (NetworkTypes) {
+    NetworkTypes["Public"] = "public";
+    NetworkTypes["Private"] = "private";
+})(NetworkTypes = exports.NetworkTypes || (exports.NetworkTypes = {}));
